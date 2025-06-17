a6b7d43d049ef2810bea227a5216a0d591835860
Luca Boltri <luca.boltri@fifthbeat.com>
2025-06-18 00:10:17 +0200

    Navbar implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1 class="text-2xl font-bold underline">FE project setup completed!</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
